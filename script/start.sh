d248241a1056c5275d1082e4db1f23fc58de10f5
Mateusz Maciaś <mmacias@piap.pl>
2020-02-03 13:30:50 +0100

    Apache can be sensitive to leftover pid file

--- script/start.sh
+++ script/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Start CRON 
+# Start CRON
 service cron start
+
+# Apache can be sensitive to leftover pid file
+rm /var/run/apache2.pid
 
 # Start apache
 /usr/sbin/apache2 -DFOREGROUND
